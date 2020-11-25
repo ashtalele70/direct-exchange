13ffdc8c0df86afe68c3f7eddb8bff9c44b545eb
ashtalele70 <ashtalele70@gmail.com>
2020-11-25 02:42:58 -0800

    User add, get, udpate - Backend APIs

--- backend/src/main/java/edu/sjsu/directexchange/dao/UserDaoImpl.java
+++ backend/src/main/java/edu/sjsu/directexchange/dao/UserDaoImpl.java
@@ -1,25 +1,37 @@
 package edu.sjsu.directexchange.dao;
 
 import edu.sjsu.directexchange.model.User;
-import org.hibernate.Session;
-import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 
 @Repository
 public class UserDaoImpl implements UserDao {
 
-  @Autowired
   private EntityManager entityManager;
+
+  @Autowired
+  public UserDaoImpl(EntityManager theEntityManager) {
+    entityManager = theEntityManager;
+  }
 
   @Override
   public User getUserById(Long id) {
-    Session currentSession = entityManager.unwrap(Session.class);
-    Query<User> query = currentSession.createQuery("from User where id =:" +
-        " id", User.class).setParameter("id", id);
-    User user = query.getSingleResult();
+    User user = entityManager.find(User.class, id);
     return user;
+  }
+
+  @Override
+  @Transactional
+  public void createUser(User user) {
+    entityManager.merge(user);
+  }
+
+  @Override
+  @Transactional
+  public void updateUser(User user) {
+    entityManager.merge(user);
   }
 }
--- backend/src/main/java/edu/sjsu/directexchange/model/User.java
+++ backend/src/main/java/edu/sjsu/directexchange/model/User.java
@@ -1,21 +1,18 @@
 package edu.sjsu.directexchange.model;
 
-
-import lombok.Getter;
-import lombok.Setter;
-
 import javax.persistence.*;
 
 @Entity
 @Table(name = "user")
-@Setter
-@Getter
 public class User {
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   @Column
   private long id;
+
+  @Column
+  private String username;
 
   @Column
   private String password;
@@ -25,4 +22,56 @@ public class User {
 
   @Column
   private boolean isVerified;
+
+  @Column
+  private String nickname;
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  public int getOauthType() {
+    return oauthType;
+  }
+
+  public void setOauthType(int oauthType) {
+    this.oauthType = oauthType;
+  }
+
+  public boolean isVerified() {
+    return isVerified;
+  }
+
+  public void setVerified(boolean verified) {
+    isVerified = verified;
+  }
+
+  public String getNickname() {
+    return nickname;
+  }
+
+  public void setNickname(String nickname) {
+    this.nickname = nickname;
+  }
+
 }
