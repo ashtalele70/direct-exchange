2a89dffb39366c2e04c5cdac71e7349a6866993d
ashtalele70 <ashtalele70@gmail.com>
2020-11-27 00:43:35 -0800

    Localhost changes

--- frontend/src/helper/communicationHelper.js
+++ frontend/src/helper/communicationHelper.js
@@ -21,7 +21,7 @@ function sendRequest(type, route, data) {
     }
 
     return $.ajax({
-        url: "http://localhost:8080/" + route,
+        url: process.env.REACT_APP_ROOT_URL_SLASH + route,
         data: formData,
         type,
         processData: false,
@@ -46,7 +46,7 @@ export function constructHttpRequest(type, route, data = null) {
                 });
             }
             return $.ajax({
-                url: "http://localhost:8080/" + paramRoute,
+                url: process.env.REACT_APP_ROOT_URL_SLASH + paramRoute,
                 type: type,
                 xhrFields: {
                     withCredentials: true,
