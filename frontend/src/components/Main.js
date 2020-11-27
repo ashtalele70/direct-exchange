d3d2e54ba58219b19be6fb8d796719f8ef07e419
terrylinda13@gmail.com <terrylinda13@gmail.com>
2020-11-26 18:45:27 -0800

    chages for add bank

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -3,19 +3,21 @@ import { Route } from "react-router-dom";
 import "../App.css";
 
 import Navbar from "./Navbar/Navbar";
-import Rates from "./Rates/Rates"
-import PostOffer from "./PostOffer/PostOffer"
+import Rates from "./Rates/Rates";
+import PostOffer from "./PostOffer/PostOffer";
+import AddBank from "./Bank/AddBank";
 
 class Home extends Component {
-    render() {
-        return (
-            <div>
-                <Route path="/" component={Navbar} />
-                <Route path="/rates" component={Rates} />
-                <Route path="/postoffer" component={PostOffer} />
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Route path="/" component={Navbar} />
+        <Route path="/rates" component={Rates} />
+        <Route path="/postoffer" component={PostOffer} />
+        <Route path="/addbank" component={AddBank} />
+      </div>
+    );
+  }
 }
 
 export default Home;
--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,34 +1,35 @@
 import React, { Component } from "react";
 import { Navbar } from "react-bootstrap";
-import Nav from 'react-bootstrap/Nav'
+import Nav from "react-bootstrap/Nav";
 import logo from "./logo.png";
 
 import { Link } from "react-router-dom";
 class Navigation extends Component {
-    render() {
-        return (
-            <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="/">
-                    <img
-                        alt=""
-                        src={logo}
-                        width="30"
-                        height="30"
-                        className="d-inline-block align-top"
-                    />{" "}
-                    Home
-
-
-                </Navbar.Brand>
-                <Nav.Link className="pl-5" href="/Rates">
-                    Exchange Rates
-                </Nav.Link>
-                <Nav.Link className="pl-5" href="/PostOffer">
-                    Post Exchange Offer
-                </Nav.Link>
-            </Navbar>
-        );
-    }
+  render() {
+    return (
+      <Navbar bg="dark" variant="dark">
+        <Navbar.Brand href="/">
+          <img
+            alt=""
+            src={logo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{" "}
+          Home
+        </Navbar.Brand>
+        <Nav.Link className="pl-5" href="/Rates">
+          Exchange Rates
+        </Nav.Link>
+        <Nav.Link className="pl-5" href="/PostOffer">
+          Post Exchange Offer
+        </Nav.Link>
+        <Nav.Link className="pl-5" href="/AddBank">
+          Add Bank
+        </Nav.Link>
+      </Navbar>
+    );
+  }
 }
 
 export default Navigation;
