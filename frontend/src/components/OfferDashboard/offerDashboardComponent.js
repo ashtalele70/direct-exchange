d83757e1368f6ae72e64c574f91e96397ec8d4cf
varsharp <my1daily1mail@gmail.com>
2020-11-28 18:04:34 -0800

    Added pagination - offer dashboard

--- frontend/src/components/OfferDashboard/offerDashboardComponent.js
+++ frontend/src/components/OfferDashboard/offerDashboardComponent.js
@@ -1,7 +1,7 @@
 
 import { useEffect, useState } from 'react';
 import { getAllOffers } from './offerDashboardService';
-import { Card, CardDeck, Container, Row, Col, Modal, Button, Dropdown, FormControl, InputGroup, Form } from 'react-bootstrap';
+import { Card, CardDeck, Container, Row, Col, Modal, Button, Dropdown, FormControl, Pagination, Form } from 'react-bootstrap';
 import ReactStars from "react-rating-stars-component";
 import { faStar, faStarHalf } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
@@ -9,7 +9,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export function OfferDashboardComponent() {
     const [offers, setOffers] = useState([]);
     const [allOffers, setAllOffers] = useState([]);
+    const [startIndex, setStartIndex] = useState(0);
+    const [active, setActivePage] = useState(1);
     let offerList = [];
+    let itemsPerPage = 10;
+    let items = [];
 
     useEffect(() => {
         async function fetchData() {
@@ -20,14 +24,18 @@ export function OfferDashboardComponent() {
         fetchData();
     }, []);
 
-    const onRatingChangeHandler = (newRating) => {
-        console.log(newRating);
-    };
+    // const onRatingChangeHandler = (newRating) => {
+    //     console.log(newRating);
+    // };
 
     const [show, setShow] = useState(false);
+    //const [showOfferDetailModal, setShowOfferDetailModel] = useState(false);
 
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
+
+    // const handleCloseOfferDetailModal = () => setShowOfferDetailModel(false);
+    // const handleShowOfferDetailModal = () => setShowOfferDetailModel(true);
 
     const getOfferList = () => {
         let list = [];
@@ -79,7 +87,22 @@ export function OfferDashboardComponent() {
         setOffers(allOffers);
     }
 
+    const pageOnClickHandler = (e) => {
+        let page = Number(e.target.innerText[0]);
+        setStartIndex((page-1)*10);
+        setActivePage(page);
+    }
+
     if (offers) {
+        let pageCount = (offers.length / itemsPerPage) + 1;
+        for (let number = 1; number <= pageCount; number++) {
+            items.push(
+                <Pagination.Item key={number} active={number === active}>
+                {number}
+                </Pagination.Item>,
+            );
+        }
+
         offerList = Object.keys(offers).map(key =>
             <Col xs={2} md={4} lg={6} className="mt-3">
                 <Card bg="light" border="secondary" className="mt-2">
@@ -89,7 +112,7 @@ export function OfferDashboardComponent() {
                             <span className="font-weight-bold">Rating: <ReactStars
                                 count={5}
                                 value={offers[key].ratings && offers[key].ratings.length > 0 ? offers[key].ratings[0].avgRating : 0}
-                                onChange={onRatingChangeHandler}
+                                // onChange={onRatingChangeHandler}
                                 size={24}
                                 isHalf={true}
                                 emptyIcon={<FontAwesomeIcon icon={faStar} />}
@@ -128,6 +151,8 @@ export function OfferDashboardComponent() {
                 </Card>
             </Col>
         );
+        
+        offerList = ((startIndex+10) < offerList.length) ? offerList.slice(startIndex, startIndex+10) : offerList.slice(startIndex);
     }
 
     return (
@@ -198,11 +223,75 @@ export function OfferDashboardComponent() {
                     </Button>
                 </Modal.Footer>
             </Modal>
+            {/* <Modal show={show} onHide={handleCloseOfferDetailModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Filter By</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Row>
+                        <Col md={5}>
+                            <Form>
+                                <Form.Group controlId="formGroupSrcCurrency">
+                                    <Form.Label>Source Currency</Form.Label>
+                                    <Dropdown id="dropdown-basic-button" onSelect={(e) => handleSrcCurrencyChange(e)}>
+                                        <Dropdown.Toggle variant="success" id="dropdown-basic">
+                                            {srcCurrency ? srcCurrency : "Please Select"}
+                                        </Dropdown.Toggle>
+                                        <Dropdown.Menu>
+                                            <Dropdown.Item eventKey="USD">USD</Dropdown.Item>
+                                            <Dropdown.Item eventKey="INR">INR</Dropdown.Item>
+                                            <Dropdown.Item eventKey="RMB">RMB</Dropdown.Item>
+                                            <Dropdown.Item eventKey="EUR">EUR</Dropdown.Item>
+                                            <Dropdown.Item eventKey="GBP">GBP</Dropdown.Item>
+                                        </Dropdown.Menu>
+                                    </Dropdown>
+                                </Form.Group>
+                                <Form.Group controlId="formGroupDestCurrency">
+                                    <Form.Label>Destination Currency</Form.Label>
+                                    <Dropdown id="dropdown-basic-button" onSelect={(e) => handleDestCurrencyChange(e)}>
+                                        <Dropdown.Toggle variant="success" id="dropdown-basic">
+                                            {destCurrency ? destCurrency : "Please Select"}
+                                        </Dropdown.Toggle>
+                                        <Dropdown.Menu>
+                                            <Dropdown.Item eventKey="USD">USD</Dropdown.Item>
+                                            <Dropdown.Item eventKey="INR">INR</Dropdown.Item>
+                                            <Dropdown.Item eventKey="RMB">RMB</Dropdown.Item>
+                                            <Dropdown.Item eventKey="EUR">EUR</Dropdown.Item>
+                                            <Dropdown.Item eventKey="GBP">GBP</Dropdown.Item>
+                                        </Dropdown.Menu>
+                                    </Dropdown>
+                                </Form.Group>
+                            </Form>
+                        </Col>
+                        <Col md={7}>
+                            <Form>
+                                <Form.Group controlId="formGroupEmail">
+                                    <Form.Label>Source Currency Amount</Form.Label>
+                                    <FormControl defaultValue={srcAmount} aria-label="Amount (to the nearest dollar)" onChange={(e) => handleSrcAmountChange(e)} />
+                                </Form.Group>
+                                <Form.Group controlId="formGroupPassword">
+                                    <Form.Label>Destination Currency Amount</Form.Label>
+                                    <FormControl defaultValue={destAmount} aria-label="Amount (to the nearest dollar)" onChange={(e) => handleDestAmountChange(e)} />
+                                </Form.Group>
+                            </Form>
+                        </Col>
+                    </Row>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClear}>
+                        Clear
+                    </Button>
+                    <Button variant="primary" onClick={handleClose}>
+                        Save Changes
+                    </Button>
+                </Modal.Footer>
+            </Modal> */}
             <CardDeck>
                 <Row className="mt-3 mb-5">
                     {offerList}
                 </Row>
             </CardDeck>
+            <Pagination className="float-right" onClick = {(e) => pageOnClickHandler(e)}>{items}</Pagination>
         </Container>
     )
 }
